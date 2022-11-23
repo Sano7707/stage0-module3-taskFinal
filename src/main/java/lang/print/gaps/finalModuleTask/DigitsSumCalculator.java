02a50050ea0f7b66070e4ea89bfe342df79ed770
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-23 20:13:55 +0400

    Changes

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,17 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void calculateSum(int number) {
+      int first = number % 10;
+      number = (number - first)/10;
+      int second = number % 10;
+      number = (number - second)/10;
+      int third = number % 10;
+      int fourth = (number - third)/10;
+        System.out.println(first+second+third+fourth);
+    }
+
+    public static void main(String[] args) {
+        calculateSum(1111);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+       int a = number % 10;
+       number = (number - a)/10;
+       int b = number % 10;
+       int c =  (number - b)/10;
+        System.out.println(a+""+b+c);
     }
 }
